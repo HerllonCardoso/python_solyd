2a62054af49cb708db2ee9d03915dbca8e71aba8
Herllon Cardoso <herllon.cardoso@alfalaval.com>
2020-08-28 14:00:38 -0300

    first py commit

---
+++ aula1-intro.py
@@ -0,0 +1,7 @@
+a = 10;
+
+b = 10;
+
+print(a+b)
+
+print("Hello World")
---
+++ aula2-entrada-e-saida.py
@@ -0,0 +1,22 @@
+print("Hello World ! \t Segundo print")
+print('segundo print')
+
+
+nome = "Herllon"
+idade = 23
+altura = 1.85
+tipo_nome = type(nome)
+
+tipo_idade = type(idade)
+tipo_altura = type(altura)
+
+
+
+print(nome)
+print(tipo_nome)
+
+print(idade)
+print(tipo_idade)
+
+print(altura)
+print(tipo_altura)
