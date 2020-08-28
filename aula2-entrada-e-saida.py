60ee0587923858b8be4b9bf003f032d87ef9da9a
Herllon Cardoso <herllon.cardoso@alfalaval.com>
2020-08-28 14:21:58 -0300

    feat: ex-aula2

--- aula2-entrada-e-saida.py
+++ aula2-entrada-e-saida.py
@@ -2,9 +2,10 @@ print("Hello World ! \t Segundo print")
 print('segundo print')
 
 
-nome = "Herllon"
-idade = 23
-altura = 1.85
+
+nome = input('Escreva seu nome: ')
+idade = input('Escreva sua idade: ')
+altura = input('Escreva sua altura: ')
 tipo_nome = type(nome)
 
 tipo_idade = type(idade)
@@ -20,3 +21,6 @@ print(tipo_idade)
 
 print(altura)
 print(tipo_altura)
+
+
+print(nome, 'tem', idade, 'anos e', altura, 'de altura')
